cfa08ba89b0e318b4c9404e1e55a0bd3707d3c90
beauwilliams <beau.williams@icloud.com>
2021-06-06 00:17:10 +1000

    Added config for java lsp. Added it to merge script. Modified my todo/fixme snippets

--- Vim/nvim/launch_jdtls.sh
+++ Vim/nvim/launch_jdtls.sh
@@ -7,6 +7,7 @@
 # If you're not using Linux you'll need to adjust the `-configuration` option
 # to point to the `config_mac' or `config_win` folders depending on your system.
 
+# NOTE: Download ecplise precompiled jar here https://ftp.fau.de/eclipse/jdtls/snapshots/
 JAR="$HOME/.config/nvim/.langservers/jdt-language-server-latest/plugins/org.eclipse.equinox.launcher_*.jar"
 GRADLE_HOME=$HOME/gradle /Library/Java/JavaVirtualMachines/jdk-14.0.1.jdk/Contents/Home/bin/java \
   -Declipse.application=org.eclipse.jdt.ls.core.id1 \
