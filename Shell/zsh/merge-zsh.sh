c66da0ddc2784f1ed9fc0372460628f686773988
beauwilliams <beau.williams@icloud.com>
2021-06-22 18:03:25 +1000

    Moving some things around

--- Shell/zsh/merge-zsh.sh
+++ Shell/zsh/merge-zsh.sh
@@ -1,5 +1,3 @@
 cp ~/.zshrc ~/Git_Downloads/Dotfiles/Shell/zsh/
 cp -r ~/.config/zsh/ ~/Git_Downloads/Dotfiles/Shell/zsh/
-cp ~/.zsh/.p10k.zsh ~/Git_Downloads/Dotfiles/Shell/zsh/powerlevel10k/
-cp ~/.zsh/.fzf.zsh ~/Git_Downloads/Dotfiles/Shell/zsh/fzf/
 echo 'merging zshrc to dotfiles'
