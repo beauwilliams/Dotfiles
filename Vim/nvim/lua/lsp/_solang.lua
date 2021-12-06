f730d5de14df992b6915c6d4fb65498d6ac4edeb
beauwilliams <beau.williams@icloud.com>
2021-12-06 22:02:28 +1100

    fix refactor bug

--- Vim/nvim/lua/lsp/_solang.lua
+++ Vim/nvim/lua/lsp/_solang.lua
@@ -3,7 +3,7 @@ local M = {}
 local lsp = require('lspconfig')
 
 M.setup = function(custom_init, custom_attach)
-lsp.tsserver.setup({
+lsp.solang.setup({
 	on_attach = custom_attach,
 	on_init = custom_init,
     cmd = {"solang", "--language-server", "--target", "ewasm" , "--importmap=hardhat=node_modules/hardhat" }
