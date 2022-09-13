1f8a447afc8b6efa7bc921c7837e95eeeeb861ac
beauwilliams <beau.williams@icloud.com>
2022-09-13 21:50:47 +1000

    feat: a working copilot install

--- Vim/nvim/lua/plugins/_aerial.lua
+++ Vim/nvim/lua/plugins/_aerial.lua
@@ -41,7 +41,7 @@ require("aerial").setup({
   --   auto    - aerial window will stay open as long as there is a visible
   --             buffer to attach to
   --   global  - same as 'persist', and will always show symbols for the current buffer
-  close_behavior = "auto",
+  -- close_behavior = "auto", NOTE: Deprecated
 
   -- Set to false to remove the default keybindings for the aerial buffer
   default_bindings = true,
